b8f3c2cd46d3a889abc02b16711053a38c029ccf
agvim <agvim@server.fake>
2013-07-10 09:48:47 +0200

    status line fonts fixes.
    The status line fonts are directly fetched from powerline repositories
    and patched so we get the newes ones.
    vim-airline (the new powerline replacement in spf13) is configured to
    use the powerline symbols
    Minor code cleanup.

--- oninstall.sh
+++ oninstall.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-#we need the vimrc files to be linked in order to avoid installing unnecessary
-#stuff and installing custom stuff
-homeshick=$HOME/.homesick/repos/homeshick/homeshick.sh
-$homeshick link cfg-vim
-wget http://j.mp/spf13-vim3 -O /tmp/spf13-vim.sh && sh /tmp/spf13-vim.sh
-$(dirname $0)/oninstall/postinstall.sh
+#install spf13 and the configured bundles
+$(dirname $0)/oninstall/installspf13.sh
+#Install statusline fonts
+$(dirname $0)/oninstall/statuslinefonts.sh
