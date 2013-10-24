f8d8e09c6beae1acbf4177c79a0b7ab04e924180
agvim <agvim@server.fake>
2013-10-24 10:26:54 +0200

    minor changes and homeshick oninstall script fixes

--- oninstall/installspf13.sh
+++ oninstall/installspf13.sh
@@ -2,5 +2,17 @@
 #we need the vimrc files to be linked in order to avoid installing unnecessary
 #stuff and installing custom stuff
 homeshick=$HOME/.homesick/repos/homeshick/homeshick.sh
+
+#clone the spf13-vim repo
+git_branch='3.0'
+git_uri='https://github.com/spf13/spf13-vim.git'
+app_name='spf13-vim'
+endpath="$HOME/.$app_name-3"
+git clone --recursive -b "$git_branch" "$git_uri" "$endpath"
+
+#before setting up plugins with bundle, link the homeshick vim config files so
+#we do not download unwanted plugins
 $homeshick link cfg-vim
-wget http://j.mp/spf13-vim3 -O /tmp/spf13-vim.sh && sh /tmp/spf13-vim.sh
+
+#run the bootstrap script to download plugins and install spf13
+$endpath/bootstrap.sh
