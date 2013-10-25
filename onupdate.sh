79d617991d032ea22ea9fea8ee092eab8599993f
agvim <agvim@server.fake>
2013-10-25 17:21:49 +0200

    changed vim diff expression and updated homeshick

--- onupdate.sh
+++ onupdate.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 #we need the vimrc files to be linked in order to avoid installing unnecessary
 #stuff and installing custom stuff
-homeshick=$HOME/.homesick/repos/homeshick/homeshick.sh
-$homeshick link cfg-vim
+
+#if the homeshick repo has an update, re-link cfg-vim files before updating vim
+if [[ $1 == 1 ]]; then
+    homeshick=$HOME/.homesick/repos/homeshick/homeshick.sh
+    $homeshick link cfg-vim
+fi
+
 #bootstrap works both for updating and for fresh setup
 $HOME/.spf13-vim-3/bootstrap.sh
+
+#clean up old views to avoid problems with updated plugins
+rm -f $HOME/.vimviews/*
