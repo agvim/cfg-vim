02d8fb3e06b769cbb18baf806cab1463420dd46d
agvim <agvim@server.fake>
2014-01-18 00:19:44 +0100

    fixes, switched to YouCompleteMe

--- oninstall.sh
+++ oninstall.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #install spf13 and the configured bundles
 $(dirname $0)/oninstall/installspf13.sh
-#Install statusline fonts
+#install statusline fonts
 $(dirname $0)/oninstall/statuslinefonts.sh
+#install YouCompleteMe
+$(dirname $0)/oninstall/youcompleteme.sh
