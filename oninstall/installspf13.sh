1fcb47cc898b2637a779b42af354fc2c0a1d0a7d
agvim <agvim@server.fake>
2014-10-15 11:55:20 +0200

    check for whether we have powerline fonts already

--- oninstall/installspf13.sh
+++ oninstall/installspf13.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
-#we need the vimrc files to be linked in order to avoid installing unnecessary
-#stuff and installing custom stuff
+# we need the vimrc files to be linked in order to avoid installing unnecessary
+# stuff and installing custom stuff
 homeshick=$HOME/.homesick/repos/homeshick/bin/homeshick
 
-#clone the spf13-vim repo
+# clone the spf13-vim repo
 git_branch='3.0'
 git_uri='https://github.com/spf13/spf13-vim.git'
 app_name='spf13-vim'
 endpath="$HOME/.$app_name-3"
 git clone --recursive -b "$git_branch" "$git_uri" "$endpath"
 
-#before setting up plugins with bundle, link the homeshick vim config files so
-#we do not download unwanted plugins
+# before setting up plugins with bundle, link the homeshick vim config files so
+# we do not download unwanted plugins
 $homeshick link cfg-vim
 
-#run the bootstrap script to download plugins and install spf13
+# run the bootstrap script to download plugins and install spf13
 $endpath/bootstrap.sh
 
-#FIXME: As long as UnBundle does not work in bootstrap, manually clean the
-#UnBundled plugins afterwards
+# FIXME: As long as UnBundle does not work in bootstrap, manually clean the
+# UnBundled plugins afterwards
 vim +BundleInstall +BundleClean! +qall
--- onupdate.sh
+++ onupdate.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-#we need the vimrc files to be linked in order to avoid installing unnecessary
-#stuff and installing custom stuff
+# we need the vimrc files to be linked in order to avoid installing unnecessary
+# stuff and installing custom stuff
 
-#if the homeshick repo has an update, re-link cfg-vim files before updating vim
+# if the homeshick repo has an update, re-link cfg-vim files before updating vim
 if [[ $1 == 1 ]]; then
     homeshick=$HOME/.homesick/repos/homeshick/bin/homeshick
     $homeshick link cfg-vim
 fi
 
-#FIXME: As long as UnBundle does not work in bootstrap, manually do the pull
-#and the vundle commands
-##bootstrap works both for updating and for fresh setup
-#$HOME/.spf13-vim-3/bootstrap.sh
+# FIXME: As long as UnBundle does not work in bootstrap, manually do the pull
+# and the vundle commands
+# #bootstrap works both for updating and for fresh setup
+# $HOME/.spf13-vim-3/bootstrap.sh
 
-#save the cwd
+# pull only the interesting spf13 branch
 currdir=`pwd`
-#pull only the interesting spf13 branch
 spf13_branch='3.0'
 cd $HOME/.spf13-vim-3 && git pull origin "$spf13_branch"
 vim +BundleInstall! +BundleClean! +qall
-#restore the cwd
 cd $currdir
 
-#clean up old views to avoid problems with updated plugins
+# clean up old views to avoid problems with updated plugins
 rm -f $HOME/.vimviews/*
