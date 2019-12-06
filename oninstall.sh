ba1532c08bf2fa67b7b4ba64cc9ba5b9f777a52c
agvim <agvim@users.noreply.github.com>
2019-12-06 12:00:30 +0100

    removed neovim stuff

--- oninstall.sh
+++ oninstall.sh
@@ -4,9 +4,3 @@ mkdir -p ~/.vim/autoload/
 wget -O ~/.vim/autoload/plug.vim -- https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 $HOME/.homesick/repos/homeshick/bin/homeshick link cfg-vim
 vim "+PlugInstall" '+qa!'
-
-# neovim compatibility
-mkdir -p ~/.local/share/nvim/site/autoload/
-ln -s ~/.vim/autoload/plug.vim ~/.local/share/nvim/site/autoload/plug.vim
-mkdir -p ~/.config/nvim/
-ln -s ~/.vimrc ~/.config/nvim/init.vim
